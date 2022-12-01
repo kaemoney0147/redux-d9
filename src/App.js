6aab6ae0e77ad00b041201995d4c1976213c924e
abiodun <114649936+kaemoney0147@users.noreply.github.com>
2022-12-01 16:16:46 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import MainSearch from './components/MainSearch'
+import CompanySearchResults from './components/CompanySearchResults'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import FavoriteList from './components/FavoriteList'
+
+function App() {
+  return (
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainSearch />} />
+        <Route path="/:companyName/" element={<CompanySearchResults />} />
+        <Route path='/favoritelist' element={<FavoriteList/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
